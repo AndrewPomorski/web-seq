9593ca7c7c2356e2fcf6c0030e58ec2d7901e5f8
andrew <andrew@shepherdcmms.com>
2020-09-23 22:07:05 +0800

    wasm-pack build works, calling from js works

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,6 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
 #[derive(Clone)]
 pub struct RollNote {
     on: bool,
@@ -16,7 +15,21 @@ pub struct RollNote {
     velocity: u8, // 0-100
 }
 
-#[wasm_bindgen]
+impl RollNote {
+    pub fn new(on: bool, trig_chance: u8, velocity: u8) -> RollNote {
+        RollNote {
+            on,
+            trig_chance,
+            velocity,
+        }
+    }
+
+    pub fn toggle(&mut self) {
+        self.on = !self.on;
+    }
+}
+
+
 #[derive(Clone)]
 pub struct RollRow {
     row: Vec<RollNote>,
@@ -25,40 +38,60 @@ pub struct RollRow {
 #[wasm_bindgen]
 pub struct WebSeq {
     tempo: u8,
-    time: u8, // 4, 8, 16, 32, 64
     bars: u8, // 2, 4, 8, 16, 32
     notes_per_bar: u8, // piano roll
     roll: Vec<RollRow>,
     step_index: u8,
+    rows: u8,
 }
 
+#[wasm_bindgen]
 impl WebSeq {
-    pub fn new(tempo: u8, time: u8, bars: u8, notes_per_bar: u8, mut roll: Vec<RollRow>, step_index: u8) -> WebSeq {
-        let roll_note = RollNote {
-            on: true,
-            trig_chance: 100,
-            velocity: 100,
-        };
+    pub fn new(tempo: u8, time: u8, bars: u8, notes_per_bar: u8, step_index: u8, rows: u8) -> WebSeq {
+        
+        let roll_note = RollNote::new(true, 100, 100);
+        
+        // How many notes per piano roll row
+        let steps: usize = (notes_per_bar * bars).into();
+        // Init one row
         let row = RollRow {
-            row: vec![roll_note; 4],
+            row: vec![roll_note; steps],
         };
-        roll = vec![row; 4];
+        
         WebSeq {
             tempo: tempo,
-            time: time,
             bars: bars,
             notes_per_bar: notes_per_bar,
-            roll: roll,
+            roll: vec![row; usize::from(rows)],
             step_index: step_index,
+            rows,
         }
     }
 
-    pub fn get_tempo(&self) -> &u8 {
-        &self.tempo
+
+    fn get_steps(&self) -> u8 {
+        self.bars * self.notes_per_bar
+    }
+
+    pub fn step(&mut self) {
+        let row_notes = self.get_steps();
+        if self.step_index == row_notes {
+            self.step_index = 0;
+        }
+        self.step_index += 1;
+    }
+
+    // Get set
+    pub fn get_tempo(&self) -> u8 {
+        self.tempo
     }
 
     pub fn set_tempo(&mut self, tempo: u8) {
         self.tempo = tempo;
+    }
+
+    pub fn render(&self) -> String {
+        format!("Ok")
     }
 
 }
